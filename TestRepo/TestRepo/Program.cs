bc0aa5dd972ce330261e357b63eeeb1018b51457
Yakov <abdreimovyam@soyuz317.ru>
2023-10-03 21:38:58 +0400

    Просто создал пустой проект

---
+++ TestRepo/TestRepo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сейчас что нибудь здесь напишем
+            Console.WriteLine("Пока ничего не сделано!");
+            Console.ReadLine();
+        }
+    }
+}
